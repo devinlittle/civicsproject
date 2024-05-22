ce4773797e8221cab1c80ebf858c3e3103d1ff18
Devin Little <pixlovesit@gmail.com>
2024-05-21 20:58:35 -0400

    Made things look good but actuly made it useful and stuff

---
+++ src/lib/comps/Card.svelte
@@ -0,0 +1,70 @@
+<!---> $lib/comps/Card.svelte <!-->
+<script>
+import leadingup from "$lib/images/leadingup.svg?raw";
+</script>
+
+<div class="cardele">
+  <div class="card">
+    <p>Over 75% of students indicated they would feel relieved if MCAS testing were permanently removed. This data suggests that a significant number of students experience negative feelings or stress related to the testing. Additionally, in response to the question, 'How do you feel leading up to MCAS testing?, 51.2% of students reported feeling 'stressed' and 44.8% felt 'annoyed'. These findings imply widespread dissatisfaction and anxiety among students regarding the MCAS testing process.</p>
+<!--  <div class="card2"> !-->
+    <br/>
+    {@html leadingup}  
+  </div>
+
+</div>
+
+
+<!---> Styles <!--> 
+
+<style>
+
+:global(svg#svg22) {
+	display: flex;
+	padding: 22px;
+	height: 13rem;
+	width: 20rem;
+	position: absolute;
+	margin-left: 601px;
+	margin-top: -234px;
+}
+
+.cardele {
+  border-bottom-style: solid;
+  border-bottom-width: 100px;
+  border-color: transparent;
+}
+.card {
+	border-radius: 25px;
+	background: var(--color-theme-1);
+	padding: 3rem;
+	width: 52rem;
+	height: 8rem;
+/* 	transform: rotate(-2.5deg); */
+}
+.card2 {
+	border-radius: 50px;
+	background: var(--color-theme-1);
+	padding: 7.5rem;
+	width: 7.5rem;
+	height: 3.1rem;
+	margin-left: 72%;
+	margin-top: -30%;
+	transform: rotate(deg);
+}
+.card3 {
+	border-radius: 25px;
+	background: var(--color-theme-1);
+	padding: 8rem;
+	width: 18rem;
+	height: 1rem;
+	margin-left: -65%;
+	margin-top: -49%;
+	transform: rotate(-8.2deg);
+}
+
+p {
+	margin-top: -25px;
+	padding-right: 20%;
+}
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,87 @@
+<script>
+  import Card from "$lib/comps/Card.svelte";
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+    MCAS Free<br/>
+		Learning, Not Testing!
+	</h1>
+</section>
+
+<div class="flexcard">
+  <Card/>
+
+
+  <div class="cardgamer">
+    <p>Most students that responded to the servey said that MCAS was a waste of time for students as they can be learning more important topics rather than recapping the moon phases. "We know right now that standardized testing is really disruptive. There are ways of minimizing that disruption" (Eden Mor, gazettenet.com)</p>
+  </div>
+
+
+  <!-- Graph/Graphc for latter -->
+
+  <div class="cardgamer2">
+    <p></p>
+ <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/TCdxSoAhB9Q?si=uuqabUOWQZLllJ22" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> 
+  
+  </div>
+
+  <!--   -->
+</div>
+
+<!--<div class="svelte">
+  {@html leadingup}
+</div> !-->
+
+<!-- Card thing 3x with graphs and stuff !-->
+
+<style>
+  .flexcard {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+  }
+
+  .cardgamer {
+	border-radius: 25px;
+	background: var(--color-theme-1);
+	padding: 2rem;
+	width: 46rem;
+	height: 8rem;
+	left: 250px;
+	position: absolute;
+	bottom: 85px;
+  }
+  
+  .cardgamer2 {
+	border-radius: 25px;
+	position: absolute;
+	background: var(--color-theme-1);
+	padding: 2rem;
+	width: 46rem;
+	height: 8rem;
+	right: 250px;
+	bottom: -200.5px;
+  }
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
